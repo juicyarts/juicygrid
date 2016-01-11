3687669dc92f44f8cf4682986315bddd1c5460a1
Hueseyin Yildiz <huess@juicyarts.de>
2016-01-11 14:58:06 +0100

    codeshop build init

--- public/js/wfb.js
+++ public/js/wfb.js
@@ -3,6 +3,16 @@ var wfb = angular.module('wfb', []);
 
 wfb.controller('menuController', ['$scope', '$window', function($scope, $window) {
 	$scope.showMenu = false;
+	$scope.fixHeader = false;
+	angular.element($window).bind("scroll", function() {
+		if($window.pageYOffset >= 120){
+			$scope.fixHeader = true;
+			$scope.$apply()
+		} else {
+			$scope.fixHeader = false;
+			$scope.$apply()
+		}
+	});
 }]);
 
 wfb.directive('toggleMenuItemSub', [function() {
@@ -12,8 +22,7 @@ wfb.directive('toggleMenuItemSub', [function() {
 		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
 		link: function($scope, iElm, iAttrs, controller) {
 			$scope.expand = false;
-
-			$scope.toggle = function(){
+			$scope.toggle = function() {
 				$scope.expand = !$scope.expand;
 				console.log('test');
 			}
